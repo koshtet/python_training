1d4586a83f6db314f1f8d70539b194060100f2c4
Morozov.Konstantin <Konstantin.Morozov@totalobjects.spb.ru>
2019-04-02 09:30:58 +0300

    Задание №7. Добавлены недостающие тесты.

---
+++ test/test_edit_contact.py
@@ -0,0 +1,8 @@
+from model.contact import Contact
+
+def test_delete_edit_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.edit_first_contact(Contact(first_name="EditedName", middle_name="EditedMiddleName", last_name="EditedLastName", nickname="EditedNickName", title="title", company="xb", address="spb", home_phone="111", mobile_phone="222", work_phone="333",
+                                   fax="444", email="1@1.com", email_2="2@2.com", email_3="3@3.com", homepage="www.homepage.com", birth_day="12", birth_month="February", birth_year="1988", anniversary_day="13",
+                                   anniversary_month="January", anniversary_year= "2019", sec_address="secAddress", sec_home="secHome", sec_notes="secNotes"))
+    app.session.logout()
