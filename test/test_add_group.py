a7c9c36e58fd6f2fb1aa8c9d574acfba35c2c130
Morozov.Konstantin <Konstantin.Morozov@totalobjects.spb.ru>
2019-04-04 11:45:23 +0300

    Создание интеллектуальной фикстуры с проверкой валидности.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,13 +3,9 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="test1", header="test1", footer="test1"))
-    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
 
